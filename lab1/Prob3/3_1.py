48a049ebc5975cd0a0af00633f4aabe5e6248351
augurier <138495178+augurier@users.noreply.github.com>
2024-04-19 09:53:50 +0800

    Update 3_1.py

--- lab1/Prob3/3_1.py
+++ lab1/Prob3/3_1.py
@@ -1,6 +1,7 @@
 import copy
 import queue
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 class points:
     def __init__(self, a, b, direct, cnt, father):
@@ -25,28 +26,28 @@ def finish(point, n, m):
     else:
         return False
     
-def up(old_point):
+def up(old_point, v):
     a = old_point.a
     b = old_point.b
-    cnt = old_point.cnt + 1
+    cnt = old_point.cnt + v
     new_point = points(a-1, b, 'u', cnt, old_point)
     return new_point
-def down(old_point):
+def down(old_point, v):
     a = old_point.a
     b = old_point.b
-    cnt = old_point.cnt + 1
+    cnt = old_point.cnt + v
     new_point = points(a+1, b, 'd', cnt, old_point)
     return new_point
-def left(old_point):
+def left(old_point, v):
     a = old_point.a
     b = old_point.b
-    cnt = old_point.cnt + 1
+    cnt = old_point.cnt + v
     new_point = points(a, b-1, 'l', cnt, old_point)
     return new_point
-def right(old_point):
+def right(old_point, v):
     a = old_point.a
     b = old_point.b
-    cnt = old_point.cnt + 1
+    cnt = old_point.cnt + v
     new_point = points(a, b+1, 'r', cnt, old_point)
     return new_point
 
@@ -57,19 +58,31 @@ def move(maze, n, m, old_point):
     direct = old_point.direct
 
     if a != 0 and direct != 'd' and maze[a-1][b] != 1:
-        ret.append(up(old_point))
+        if maze[a][b] == 2 or maze[a][b] == 5:
+            ret.append(up(old_point, 2))
+        else:
+            ret.append(up(old_point, 1))
     if b != 0 and direct != 'r' and maze[a][b-1] != 1:
-        ret.append(left(old_point))
+        if maze[a][b] == 2 or maze[a][b] == 5:
+            ret.append(left(old_point, 2))
+        else:
+            ret.append(left(old_point, 1))
     if a != n-1 and direct != 'u' and maze[a+1][b] != 1:
-        ret.append(down(old_point))    
+        if maze[a][b] == 2 or maze[a][b] == 5:
+            ret.append(down(old_point, 2))
+        else:
+            ret.append(down(old_point, 1))   
     if b != m-1 and direct != 'l' and maze[a][b+1] != 1:
-        ret.append(right(old_point))            
+        if maze[a][b] == 2 or maze[a][b] == 5:
+            ret.append(right(old_point, 2))
+        else:
+            ret.append(right(old_point, 1))           
     return ret
     
 
 def dijkstra(maze, n, m):
     q = queue.PriorityQueue()
-    point0 = points(0, 0, 'd', 0, None)
+    point0 = points(0, 0, '', 0, None)
     q.put(point0)
     cover = set()
     cover.add((point0.a, point0.b))
@@ -80,11 +93,11 @@ def dijkstra(maze, n, m):
             return old_point
         
         new_points = move(maze, n, m, old_point)
+        maze[old_point.a][old_point.b] += 3
         for new_point in new_points:
             if (new_point.a,new_point.b) not in cover:
                 q.put(new_point)
                 cover.add((new_point.a,new_point.b))
-                maze[new_point.a][new_point.b] = 0.5
                 
     print('no path exist!')
     return None
@@ -93,14 +106,14 @@ def dfs(maze, n, m):
     min = n*m
     ret = None
     q = []
-    point0 = points(0, 0, 'd', 0, None)
+    point0 = points(0, 0, '', 0, None)
     cover0 = set()
     cover0.add((point0.a, point0.b))    
     q.append((point0, cover0))
 
     
     while(len(q) > 0):
-        old_point, cover = q.pop()
+        old_point, cover = q.pop()    
         if(finish(old_point, n, m)):
             if old_point.cnt < min:
                 min = old_point.cnt
@@ -108,44 +121,54 @@ def dfs(maze, n, m):
             continue
         
         new_points = move(maze, n, m, old_point)
+        if maze[old_point.a][old_point.b] < 3:
+            maze[old_point.a][old_point.b] += 3
         new_cover = copy.deepcopy(cover)
         for new_point in new_points:
             if (new_point.a,new_point.b) not in cover:
                 new_cover.add((new_point.a,new_point.b))
                 q.append((new_point, new_cover))                
-                maze[new_point.a][new_point.b] = 0.5
     if(ret == None):            
         print('no path exist!')
     return ret
 
 def bfs(maze, n, m):
-    q = []
-    point0 = points(0, 0, 'd', 0, None)
-    q.append(point0)
-    cover = set()
-    cover.add((point0.a, point0.b))
+    min = n*m
+    ret = None
+    q = queue.Queue()
+    point0 = points(0, 0, '', 0, None)
+    cover0 = set()
+    cover0.add((point0.a, point0.b))    
+    q.put((point0, cover0))
+
     
-    while(len(q) > 0):
-        old_point = q.pop(0)
+    while(not q.empty()):
+        old_point, cover = q.get()    
         if(finish(old_point, n, m)):
-            return old_point
+            if old_point.cnt < min:
+                min = old_point.cnt
+                ret = old_point
+            continue
         
         new_points = move(maze, n, m, old_point)
+        if maze[old_point.a][old_point.b] < 3:
+            maze[old_point.a][old_point.b] += 3
+        new_cover = copy.deepcopy(cover)
         for new_point in new_points:
             if (new_point.a,new_point.b) not in cover:
-                q.append(new_point)
-                cover.add((new_point.a,new_point.b))
-                maze[new_point.a][new_point.b] = 0.5
-                
-    print('no path exist!')
-    return None
+                new_cover.add((new_point.a,new_point.b))
+                q.put((new_point, new_cover))                
+    if(ret == None):            
+        print('no path exist!')
+    return ret
+
 
 def mahattan(n, m, a, b):
     return n-1-a + m-1-b
 
 def astar(maze, n, m):
     q = queue.PriorityQueue()
-    point0 = points2(0, 0, 'd', 0, None, mahattan(n, m, 0, 0))
+    point0 = points2(0, 0, '', 0, None, mahattan(n, m, 0, 0))
     q.put(point0)
     cover = set()
     cover.add((point0.a, point0.b))
@@ -156,13 +179,13 @@ def astar(maze, n, m):
             return old_point
         
         new_points = move(maze, n, m, old_point)
+        maze[old_point.a][old_point.b] += 3
         for new_point0 in new_points:
             value = mahattan(n, m, new_point0.a, new_point0.b) + new_point0.cnt
             new_point = points2(new_point0.a, new_point0.b, new_point0.direct, new_point0.cnt, new_point0.father, value)
             if (new_point.a,new_point.b) not in cover:
                 q.put(new_point)
                 cover.add((new_point.a,new_point.b))
-                maze[new_point.a][new_point.b] = 0.5
                 
     print('no path exist!')
     return None
@@ -179,7 +202,11 @@ def traceback_path(point):
 def visualize_maze_with_path(maze, path, algorithm, pos):
     plt.subplot(2, 2, pos)
     #plt.figure(figsize=(len(maze[0]), len(maze)))
-    plt.imshow(maze, cmap='Greys', interpolation='nearest')
+    colors = ['white', 'black', 'blue', 'yellow', 'green']
+    bounds = [0,1,2,3,4,6]
+    cmap = mpl.colors.ListedColormap(colors)
+    norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+    plt.imshow(maze, cmap=cmap, norm=norm, interpolation='none')
 
     if path:
         path_x, path_y = zip(*path)
@@ -200,7 +227,11 @@ n, m = map(int, input().split(' '))
 maze = []
 for i in range(n):
     str0 = input()
-    line = [int(x) for x in str0.split(' ')]
+    line = []
+    for x in str0.split(' '):
+        if x != '0' and x != '1':
+            x = '2'
+        line.append(int(x))
     maze.append(line)
 #print(maze)
 
@@ -228,3 +259,26 @@ path4 = traceback_path(ans_point4)
 visualize_maze_with_path(maze4, path4, "astar", 4)
 
 plt.show()
+
+'''
+5 5
+0 0 0 0 0
+1 2 1 1 0
+0 0 0 0 0
+0 1 1 1 1
+0 0 0 0 0
+
+5 5
+0 0 0 0 0
+1 1 1 1 5
+0 0 0 0 0
+10 10 0 0 0
+0 10 0 0 0
+
+5 5
+0 0 0 0 0
+0 1 0 1 0
+2 0 0 0 0
+0 1 0 1 0
+0 0 0 0 0
+'''
